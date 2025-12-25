75778c3345d90e642e7bb27f45d2d45def00ed71
hehezhou <hehezhou233@gmail.com>
2025-12-25 21:24:20 +0800

    add manually swap buildprobeside

--- src/include/duckdb/common/numa_config.hpp
+++ src/include/duckdb/common/numa_config.hpp
@@ -4,23 +4,22 @@
 
 #include "sys/time.h"
 
-static double GetNow() {
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
-}
-
 static int split_probe_rest;
 static int split_probe_rest_start;
-const int thread_count = 96;
 static double numa_test_start;
+extern int swap_bitmask_start;
+extern int swap_bitmask;
 
-static constexpr const duckdb::idx_t STEAL_CHUNKS = 5;
-static constexpr const duckdb::idx_t LOCAL_AT_LEAST = 10;
-
+static double GetNow() {
+	struct timeval tv;
+	gettimeofday(&tv, nullptr);
+	return tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+}
 static void InitParams() {
     split_probe_rest = split_probe_rest_start;
+    swap_bitmask = swap_bitmask_start;
 }
 
+const int thread_count = 96;
 #define ONE_SOCKET_ENABLED
 extern int socket0_cpus;
